5f955c04f14ccdf9f5ee66e55d9c356459b73271
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-06-07 22:43:46 +0800

    update

--- Wallwrap/Program.cs
+++ Wallwrap/Program.cs
@@ -1,34 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
+// using System.Collections.Generic;
+// using System.Linq;
+using System.Text;
+// using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace BingWallpaperTest
+namespace Wallwrap
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Thread.Sleep(30000); // delay
             setWallpaper();
         }
 
-        /**
-*获取壁纸网络地址 别的不多说了直接扔出代码
-*/
         public static string getURL()
         {
-            string InfoUrl = "http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1";
+            string InfoUrl = "http://www.bing.com/HPImageArchive.aspx?idx=0&n=1";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(InfoUrl);
             request.Method = "GET"; request.ContentType = "text/html;charset=UTF-8";
             string xmlDoc;
-            //使用using自动注销HttpWebResponse
+            // using using to deal HttpWebResponse
             using (HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse())
             {
                 Stream stream = webResponse.GetResponseStream();
@@ -37,45 +36,36 @@ namespace BingWallpaperTest
                     xmlDoc = reader.ReadToEnd();
                 }
             }
-            // 使用正则表达式解析标签（字符串），当然你也可以使用XmlDocument类或XDocument类
+            // Regular expression
             Regex regex = new Regex("<Url>(?<MyUrl>.*?)</Url>", RegexOptions.IgnoreCase);
             MatchCollection collection = regex.Matches(xmlDoc);
-            // 取得匹配项列表
+            // if 768p
             string ImageUrl = "http://www.bing.com" + collection[0].Groups["MyUrl"].Value;
-            if (true)
-            {
-                ImageUrl = ImageUrl.Replace("1366x768", "1920x1080");
-            }
-            return ImageUrl;
+            return ImageUrl.Replace("1366x768", "1920x1080");
         }
 
         public static void setWallpaper()
         {
-            string ImageSavePath = @"D:\Program Files\BingWallpaper";
-            //设置墙纸
-            Bitmap bmpWallpaper;
+            string ImageSavePath = Path.Combine(Path.GetTempPath(), "Wallwrap");
             WebRequest webreq = WebRequest.Create(getURL());
             //Console.WriteLine(getURL());
             //Console.ReadLine();
             WebResponse webres = webreq.GetResponse();
             using (Stream stream = webres.GetResponseStream())
             {
-                bmpWallpaper = (Bitmap)Image.FromStream(stream);
+                Bitmap bmpWallpaper = (Bitmap)Image.FromStream(stream);
                 //stream.Close();
                 if (!Directory.Exists(ImageSavePath))
                 {
                     Directory.CreateDirectory(ImageSavePath);
                 }
-                //设置文件名为例：bing2017816.jpg
-                bmpWallpaper.Save(ImageSavePath + "\\bing" + DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + ".jpg", ImageFormat.Jpeg); //图片保存路径为相对路径，保存在程序的目录下
+                bmpWallpaper.Save(ImageSavePath + "\\bing" + DateTime.Now.ToString("yyyyMMdd") + ".jpg", ImageFormat.Jpeg);
             }
-            //保存图片代码到此为止，下面就是
-            string strSavePath = ImageSavePath + "\\bing" + DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + ".jpg";
+            string strSavePath = ImageSavePath + "\\bing" + DateTime.Now.ToString("yyyyMMdd") + ".jpg";
             setWallpaperApi(strSavePath);
         }
 
 
-        //利用系统的用户接口设置壁纸
         [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
         public static extern int SystemParametersInfo(
                 int uAction,
